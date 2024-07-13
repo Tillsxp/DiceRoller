9003df79a54a002e22a8188ac5dba874a7148816
Matilda Söderberg <matilda.89@live.se>
2024-07-13 08:22:13 +0200

    Logic added

--- diceroller/src/components/DiceRoll.js
+++ diceroller/src/components/DiceRoll.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix } from '@fortawesome/free-solid-svg-icons'
 
 const DiceRoll = () => {
+
+    const [number, setNumber] = useState(1);
+    const [numberTwo, setNumberTwo] = useState(1);
+
+    const randomNum = () => {
+        return Math.floor(Math.random() * 6) + 1;
+    };
+
+    const handleClick = () => {
+        setNumber(randomNum);
+        setNumberTwo(randomNum);
+    }
+
+    const diceIcons = [
+        null,
+        faDiceOne,
+        faDiceTwo,
+        faDiceThree,
+        faDiceFour,
+        faDiceFive,
+        faDiceSix
+    ];
+
+    const getDiceIcon = (number) => {
+        return diceIcons[number]
+
+    }
+
+    const diceIcon = getDiceIcon(number);
+    const diceTwoIcon = getDiceIcon(number);
+
+
     return (
         <>
             <div>
-
+                <FontAwesomeIcon icon={diceIcons[number]} />
             </div>
             <div>
-
+                <FontAwesomeIcon icon={diceIcons[numberTwo]} />
             </div>
-            <button>Roll Dice!</button>
+            <button onClick={handleClick}>Roll Dice!</button>
         </>
     )
 }
