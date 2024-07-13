59b5358239b7470672603a96a9e42844372e54b0
Matilda Söderberg <matilda.89@live.se>
2024-07-13 07:51:24 +0200

    HTML setup + FontAwesome installed

---
+++ diceroller/src/components/DiceRoll.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix } from '@fortawesome/free-solid-svg-icons'
+
+const DiceRoll = () => {
+    return (
+        <>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <button>Roll Dice!</button>
+        </>
+    )
+}
+
+export default DiceRoll;
